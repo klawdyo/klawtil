966c518cf4b72a8e24d63a0eba4e0d5c0ba4644e
Claudio Medeiros <klawdyo@gmail.com>
2026-01-15 19:13:28 -0300

    build: troca o target do typescript

--- src/main.ts
+++ src/main.ts
@@ -1143,21 +1143,16 @@ export function timeUUID() {
     .slice(0, 16)
     .padEnd(16, "0");
 
-  // console.log({ group12, milliseconds, perf16 });
-
   // Os demais caracteres do 3, o 4 e o quinto.
   const group345 = [milliseconds, perf16].join("");
 
   const hex1 = parseInt(group12, 10).toString(16);
   const hex2 = parseInt(group345, 10).toString(16);
-  // console.log({ group12, group345 });
 
   const len = 31 - hex1.length - hex2.length;
   const rand = Math.floor(Math.random() * Math.pow(10, len))
     .toString()
     .padStart(len, "0");
-
-  // console.log({ hex1, hex2, rand, len });
 
   const hex = hex1 + hex2 + rand;
 
